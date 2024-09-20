d17932e764b3a4a268525a5b5514c3a1d2bbcbae
Zino Meyer <zino.meyer@code.berlin>
2024-09-19 18:21:37 -0700

    create repo

---
+++ explored.py
@@ -0,0 +1,26 @@
+"""
+Class for maintaining explored sets
+"""
+
+class Explored(object):
+    "Maintain an explored set."
+
+    def __init__(self):
+        "__init__() - Create an empty explored set"
+
+
+    def exists(self, state):
+        """
+        exists(state) - Has this state already been explored?
+        :param state:  
+        :return: True if already seen, False otherwise
+        """
+        
+
+    def add(self, state):
+        """
+        add(state) - Add a given state to the explored set
+        :param state:  
+        :return: None
+        """
+
